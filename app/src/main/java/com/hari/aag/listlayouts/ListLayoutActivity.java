f53bd998da7ce2ddc6a1f5a5b842328e19076dd0
harinivaskumar <harinivaskumar@live.com>
2016-12-17 06:02:12 +0530

    Code Commit
    Initial code commit

---
+++ app/src/main/java/com/hari/aag/listlayouts/ListLayoutActivity.java
@@ -0,0 +1,13 @@
+package com.hari.aag.listlayouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_layout);
+    }
+}
